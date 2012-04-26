7a2e734aab60a21c0110cf8d7b41732c92cf1371
Shion Guha <shionguha@gmail.com>
2012-04-26 11:14:08 -0400

    updated generate-loc-seeddata.rb
    faster code, better output

--- seeddata/generate-loc-seeddata.rb
+++ seeddata/generate-loc-seeddata.rb
@@ -1,35 +1,15 @@
-# TODO: keep the intro short (e.g., https://github.com/maphub/maphub-seeddata/blob/master/scripts/imagedownload.rb)
-
-# Production script for maphub. Script description as follows:
-# We need a script that generates a seed data file for the Library of Congress Maphub instance.
-# Each "map record" in the seed data file includes:
-# - pointers to the map image file URIs
-# - selected metadata fields (title, description, subject, creator)
-# 
-# We already have the maps in place and scripts to download metadata from the LoC's GMD collection (see scripts directory). The script has to read these map identifiers, iterate over the harvested metadata records, identify matching records (based on the map identifier), and output a maphub map record for each match.
-# The challenging part of this script is to select the appropriate metadata fields from the OAI-PMH records. We want only those that carry "relevant" semantics about the map. Also some data cleansing (whitespace, special chars, etc.) steps might be necessary. At the end the metadata need to be indexed by Apache Solr / Lucene.
-# The results should be a script generate-loc-seeddata which takes the directory of map image files and a directory of XML files (= the metadata records) and a set of identifiers (probably a TXT file) as input and generates an outputfile loc-seeddata.yaml
-# 
-# Possible execution:
-# generate-loc-seeddata maps/ metadata/*.xml
-# generate-loc-seeddata -n 10 maps/ metadata/*.xml for only 10 maps
-# Script Development: April 2012 by Shion Guha, Cornell University (as part of maphub project led by Dr. Bernhard Haslhofer)
-# Last Change: April 8, 2012
-
-#Setting up practice space for passing parameters {mapimgdir, metadatadir, numberofsamples}
-# Algorithm:
-# 1. Parameters are passed into the script
-# 2. An array of identifiers are built as per {numberofsamples} from the {imagedir}
-# 3. Looping {numberofsamples} times:
-# 4. The correct metadata file from {metadatadir} is identified with the current identifier.
-# 5. The metadata XML is parsed and the needed information {identifier,title,author,subject,date} is extracted.
-# 6. The output YAML file is appended with the information in the proper format.
+#!/usr/bin/env ruby
+# Name          generate-loc-seeddata.rb
+# Description   Generates yaml file with selected metadata from a directory of metadata (*.xml) and directory of maps (images.jp2)
+# Author        Shion Guha
+# Date          April 26, 2012 (last modified)
 
 #require + libraries section
 require 'optparse'
 require 'ostruct'
 require 'rexml/document'
 require 'yaml'
+
 include REXML
 include YAML
 
@@ -97,21 +77,18 @@ options = OptionParser.parse(ARGV)
 mapsimgdir = options.mapimgdir
 metadatadir = options.metadata
 numsamples = options.num.to_i
-numsamples1 = numsamples - 1 #TODO: do you really need two vars here?
 
 # 1. Iterate over all files in the given directory. Grab file names -.*.mm2
 # and store them in some data structure. Grabs only numsamples number of filenames
 
 #getting basedirectory
-mapdir = Dir.pwd + "/" + mapsimgdir # TODO: don't assume that the maps dir is always a subdir of the current dir
-
-# TODO: why not simply list all files in the dir and add them to an array?
+mapdir = Dir.pwd + "/" + mapsimgdir
 
 #getting contents of the mapdir, removing extension, printing eachfilename
 mapfilenames = Dir.glob(File.join(mapdir, '*.jp2'))
 basefile = Array.new()
 maplength = mapfilenames.length
-counter = 0	# TODO: you don't need a counter; you have the array
+counter = 0	
 
 puts "Finding all the map files..."
 #doing some fancy manipulations to get an array of id names.
@@ -119,21 +96,13 @@ for file in mapfilenames
 	eachmapfile = File.basename(file,".jp2")
 	if counter <= maplength - 1
 		basefile.insert(counter,eachmapfile)
-		counter = counter + 1
+		counter = counter +=1
 	else
 	end
 end
-
-#able to get final array of map names without extension. Success !
-# puts basefile[10]
-# puts basefile[11]
-# return
+puts "Done !"
 
 puts "Finding all the metadata files ..."
-
-# TODO: what about writing a function that stores all filenames in array?
-# Avoid duplicated code
-
 # 2. Iterate within each file in a given directory and match previous filenames
 # stored in data structure with individual metadata record.
 metadir = Dir.pwd + "/" + metadatadir
@@ -149,13 +118,13 @@ for file1 in metafilenames
 	eachmetafile = File.basename(file1)
 	if counter1 <= metalength - 1
 		metarray.insert(counter1,eachmetafile)
-		counter1 = counter1 + 1
+		counter1 = counter1 +=1
 	else
 	end
 end
 
 #able to get final array of all metadata files ! Success !
-#puts metarray
+puts "Done !"
 
 # 3. Build YAML file based on given structure from matched map filename
 # and metadata.
@@ -164,67 +133,90 @@ end
 #Success in reading particular elements. Now, lets see if the mapids can be matched.
 i = 0
 j = 0
+k = 1
 metlength = metarray.length - 1
 puts "Creating output YAML File"
-#Firstly, looping over numsamples - 1 :: i.e. for every sample requested.
 
-# TODO: avoid excessive IO -> in the worst case you are opening and closing the
-# output file (no_metadata_records * no_maps) times
-
-# TODO: simply open the output file and the then within the block, iterate over
-# all metadata xml files; in each metadata xml file, iterate over all records;
-# if a record identifes an item that is in the list of known map file names,
-# extract the defined record fields and write them to the output file.
-
-while j <= metlength
-	while i <= numsamples1
-		k = i + 1
-		# Trying to match mapids
-		#Stupid preceding string in the <identifier> tag and building full identifier thingy
-		baseid = "oai:lcoa1.loc.gov:loc.gmd/"
-		mapid = basefile[i]
-		fullid = baseid + mapid
-		#puts "This is the image id number: " + i.to_s() + " and its id is " + fullid #Success in looping over all ids.
-		#Opening current metadatafile with xml stuff
-		xmlfile = File.new(metadir + "/" + metarray[j] )
+#Trying out method with file.open
+File.open("loc-seeddata.yaml","w") do |f|
+	#iterating over all metadata files
+	metarray.each{|m| 
+	
+		#Building xml document for current metadata file
+		xmlfile = File.new(metadir +"/" + m)
 		xmldoc = Document.new(xmlfile)
-		#puts "The current metadata file being parsed is: " + metarray[j] #Success ! Now, need to parse every unique xml file and build yaml stuff.
-		identifier_nodes = XPath.match(xmldoc, "//identifier[text()='oai:lcoa1.loc.gov:loc.gmd/"+mapid+"']")
-		#puts identifier_nodes
-		record_node = identifier_nodes.first.parent.parent
-		#puts record_node Success !
-		#Now to get everything else.
-		begin
-			titleelement = record_node.elements['metadata/mods/titleInfo/title'].text #Success ! Getting title works.
-		rescue
-			titleelement = nil
-			puts "Title not found in record ..."
+	    idregex = %r|oai:lcoa1.loc.gov:loc.gmd/|
+	    #baseid = "oai:lcoa1.loc.gov:loc.gmd"
+		curids = Array.new()
+		$arrayid = Array.new()
+		counter2 = 0
+		
+		#Getting an array of identifier nodes by regexing from the full xmldoc
+		identifier_nodes = XPath.each(xmldoc, "//identifier") {|element| 
+				if element.text=~idregex
+					curids.insert(counter2,element)
+					counter2 +=1
+				end	
+		}
+		#iterating over ids
+		for ids in curids
+			
+			#Getting the <record> tag level
+			record_node = ids.first.parent.parent.parent
+						
+			#processing identifier
+			$idstr = ids.to_s
+			$arrayid = $idstr.split(%r{\/})
+			id = $arrayid[1].delete("<")
+					
+							
+			#Creating yaml file structures
+			begin
+				titleelement = record_node.elements['metadata/mods/titleInfo/title'].text 
+			rescue
+				titleelement = nil
+				puts "Title not found in record ..."
+			end
+			#getting author
+			begin
+				nameelement = record_node.elements['metadata/mods/name/namePart'].text
+			rescue
+				nameelement = nil
+				puts "Author name not found in record ..."
+			end
+			#getting date
+			begin
+				dateelement = record_node.elements['metadata/mods/originInfo/dateIssued'].text
+			rescue
+				dateelement = nil
+				puts "Date Issued not found in record ..."
+			end
+			#getting subject with authority lcsh
+			begin
+				subjectelement = record_node.elements['metadata/mods/subject[@authority="lcsh"]/topic'].text
+			rescue
+				subjectelement = nil
+				puts "Subject tags from LCSH not found in record..."
+			end
+			#Building yaml structure
+			sampleyaml = {"map" => k, "id" => id, "title" => titleelement, "author" => nameelement, "subject" => subjectelement, "date" => dateelement}
+			#writing to file
+			#puts "Writing record to output file..."
+			f.write(sampleyaml.to_yaml)
+			
+			print "Writing " + k.to_s + " of " + numsamples.to_s + " records...\n"
+			#STDOUT.flush
+			
+			k +=1
+			#checking k against numsamples and exiting if numsamples reached.
+			
+			if k > numsamples
+				puts "All Done !"
+				Process.exit
+			end
+			
 		end
-		# getting author
-		begin
-			nameelement = record_node.elements['metadata/mods/name/namePart'].text
-		rescue
-			nameelement = nil
-			puts "Author name not found in record ..."
-		end
-		# getting date
-		begin
-			dateelement = record_node.elements['metadata/mods/originInfo/dateIssued'].text
-		rescue
-			dateelement = nil
-			puts "Date Issued not found in record ..."
-		end
-		# getting subject with authority lcsh
-		begin
-			subjectelement = record_node.elements['metadata/mods/subject[@authority="lcsh"]/topic'].text
-		rescue
-			subjectelement = nil
-			puts "Subject tags from LCSH not found in record..."
-		end
-		sampleyaml = {"map" => k, "id" => mapid, "title" => titleelement, "author" => nameelement, "subject" => subjectelement, "date" => dateelement}
-		File.open('loc-seeddata.yaml',"a") {|f| f.write(sampleyaml.to_yaml)} #Success ! YAML file built
-		puts "Writing sample no: " + k.to_s() + " and its selected metadata to the output YAML file ..."
-		i+= 1
-	end
-j+=1
+	} # end metadata
+	#puts "done with metadata"
 end
+#puts "done with file"
